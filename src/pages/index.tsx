c378ee47ed578b58561b81d55ee6e2af9587abab
André Alvim <alvim@zeero.tech>
2021-12-05 18:46:55 -0300

    feat: add SignIn page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,56 @@
+import {
+  Flex,
+  Input,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
+
 export default function Home() {
-  return <div>dashgo</div>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxW={360}
+        background="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bg="gray.900"
+              variant="filled"
+              _hover={{ bg: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="text"
+              focusBorderColor="pink.500"
+              bg="gray.900"
+              variant="filled"
+              _hover={{ bg: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
