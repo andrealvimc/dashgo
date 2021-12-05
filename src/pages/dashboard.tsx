9fa43a2bea0c2101b25bf9ae3e41262016fcc8ee
André Alvim <alvim@zeero.tech>
2021-12-05 19:40:05 -0300

    feat: add sibebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,14 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
-  return <Header />;
+  return (
+    <Flex flexDir="column" h="100vh">
+      <Header />
+      <Flex w="100%" my="6" px="6" maxW={1480} mx="auto">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 }
